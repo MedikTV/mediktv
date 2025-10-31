71f21ec2c73ba6b3ef31f23d7bb313e5690dc34c
MedikTV <petrskarvan@seznam.cz>
2025-10-31 17:50:54 +0100

    Update effect.js

--- effect.js
+++ effect.js
@@ -1,17 +1,1 @@
-// Preloader
-let percent = 0;
-const preloader = document.getElementById('preloader');
-const mainContent = document.getElementById('main-content');
-const percentText = document.getElementById('percent');
-const progressBar = document.getElementById('loader-progress');
 
-const interval = setInterval(() => {
-    percent++;
-    percentText.textContent = percent + '%';
-    progressBar.style.width = percent + '%';
-    if(percent >= 100) {
-        clearInterval(interval);
-        preloader.style.display = 'none';
-        mainContent.style.display = 'block';
-    }
-}, 30);
