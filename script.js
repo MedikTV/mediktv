9773825dcadf0cf4161131ac85f03403554412a3
MedikTV <petrskarvan@seznam.cz>
2025-11-09 22:30:02 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const pyosImg = document.getElementById("pyos-img");
+    const lightbox = document.getElementById("lightbox");
+    const lightboxImg = document.getElementById("lightbox-img");
+
+    pyosImg.addEventListener("click", () => {
+        lightboxImg.src = pyosImg.src; // použije stejný obrázek
+        lightbox.style.display = "flex";
+    });
+
+    lightbox.addEventListener("click", () => {
+        lightbox.style.display = "none"; // zavření kliknutím kamkoliv
+    });
+});
